a6ff134283174632615b8f4de811c7ce6fbc40b3
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-07 11:01:30 -0400

    Set up variables for express, app, and PORT

---
+++ public/server.js
@@ -0,0 +1,9 @@
+// Dependencies
+//=======================================================
+
+const express = require("express");
+
+// Sets up Express App
+//=======================================================
+const app = express();
+const PORT = process.env.PORT || 3500;
