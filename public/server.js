66376047879893f1a9f3bdff375072c24d607dd2
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-07 11:09:31 -0400

    Set up the Express application to handle data parsing

--- public/server.js
+++ public/server.js
@@ -1,9 +1,19 @@
 // Dependencies
 //=======================================================
 
-const express = require("express");
+var express = require("express");
 
 // Sets up Express App
 //=======================================================
-const app = express();
-const PORT = process.env.PORT || 3500;
+var app = express();
+var PORT = process.env.PORT || 3500;
+
+// Set up the Express app to handle data parsing
+//=======================================================
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
