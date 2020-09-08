dae0aa7b9e7c415540d24c71c9f800cb63b98071
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-08 16:12:52 -0600

    Updated code on file

--- data/notes.js
+++ data/notes.js
@@ -1,5 +1,47 @@
-// Store the data in an array.
-var notesArray = [];
+// Dependencies
+// =============================================================
+const util = require("util")
+const fs = require("fs")
+const { v4: uuidv4 } = require("uuid")
+const readFileSync = util.promisify(fs.readFile)
+const writeFileSync = util.promisify(fs.writeFile)
 
-// Make the array accessible to other files using require.
-module.exports = notesArray;
+
+class Notes{
+    read(){
+        return readFileSync("db/db.json", "utf8")
+    }
+    write(note){
+        return writeFileSync("db/db.json", JSON.stringify(note))
+    }
+    getAllNotes(){
+        return this.read().then(notes => {
+            let viewedNotes;
+
+            try {
+                viewedNotes = [].concat(JSON.parse(notes))
+            } catch (err) {
+                viewedNotes = [];
+            }
+            return viewedNotes;
+        })
+    }
+    addNote(note){
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new error("Note can not be blank")
+        }
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
+        return this.getAllNotes().then(notes => [...notes, newNote]).then(updatedNotes => this.write(updatedNotes)).then(() => newNote)
+    }
+    removeNote(id) {
+        return this.getAllNotes().then(notes => notes.filter((note) => note.id !== id)).then(filterNotes => this.write(filterNotes))
+    }
+
+}
+module.exports = new Notes();
